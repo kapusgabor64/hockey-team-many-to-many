495623efb8d55daab623aa2b2a6ef8d64f46ce79
Gabor <kapusgabor64@gmail.com>
2024-03-25 09:57:04 +0100

    Initial commit

---
+++ src/main/java/hu/webler/hockeyteammanytomany/HockeyTeamManyToManyApplication.java
@@ -0,0 +1,13 @@
+package hu.webler.hockeyteammanytomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HockeyTeamManyToManyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HockeyTeamManyToManyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/webler/hockeyteammanytomany/HockeyTeamManyToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.webler.hockeyteammanytomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HockeyTeamManyToManyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
